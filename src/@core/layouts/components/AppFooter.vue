90061f1a91d807589d14de0e47a508ad2c8e4a07
ramafikri02 <mkaiden.fikri@gmail.com>
2022-05-19 16:59:55 +0700

    f

--- src/@core/layouts/components/AppFooter.vue
+++ src/@core/layouts/components/AppFooter.vue
@@ -1,6 +1,6 @@
 <template>
   <p class="clearfix mb-0">
-    <span class="float-md-left d-block d-md-inline-block mt-25">
+    <span class="float-md-right d-block d-md-inline-block mt-25">
       COPYRIGHT  © {{ new Date().getFullYear() }}
       <b-link
         class="ml-25"
@@ -10,13 +10,13 @@
       <span class="d-none d-sm-inline-block">, All rights Reserved</span>
     </span>
 
-    <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
+    <!-- <span class="float-md-right d-none d-md-block">Hand-crafted &amp; Made with
       <feather-icon
         icon="HeartIcon"
         size="21"
         class="text-danger stroke-current"
       />
-    </span>
+    </span> -->
   </p>
 </template>
 
