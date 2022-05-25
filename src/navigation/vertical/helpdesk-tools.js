1ee5004c669ceffdb6c9aadaeee7e4b2ec7f7665
ramafikri02 <mkaiden.fikri@gmail.com>
2022-05-25 17:08:20 +0700

    25 / 05 / 2022 v1

---
+++ src/navigation/vertical/helpdesk-tools.js
@@ -0,0 +1,88 @@
+export default [
+  {
+    title: 'Helpdesk Tools',
+    icon: 'FolderIcon',
+    children: [
+      {
+        title: 'Rubah Alamat',
+        route: null,
+      },
+      {
+        title: 'Rubah Keterangan Barang',
+        route: null,
+      },
+      {
+        title: 'Rubah Layanan',
+        route: null,
+      },
+      {
+        title: 'Rubah Destinasi',
+        route: null,
+      },
+      {
+        title: 'Rubah Tanggal',
+        route: null,
+      },
+      {
+        title: 'Rubah Berat',
+        route: null,
+      },
+      {
+        title: 'Rubah Akun',
+        route: null,
+      },
+      {
+        title: 'Rubah Transaksi Cash ke Credit',
+        route: null,
+      },
+      {
+        title: 'Rubah Status POD',
+        route: null,
+      },
+      {
+        title: 'Rubah Biaya Kirim',
+        route: null,
+      },
+      {
+        title: 'Rubah Biaya Tambahan',
+        route: null,
+      },
+      {
+        title: 'Rubah Nama Penerima',
+        route: null,
+      },
+      {
+        title: 'Rubah Nama Pengirim',
+        route: null,
+      },
+      {
+        title: 'Rubah Alamat Pengirim',
+        route: null,
+      },
+      {
+        title: 'Tracking Penggunaan Helpdesk Tools',
+        route: null,
+      },
+      {
+        title: 'Rubah Transaksi Credit ke COD',
+        route: null,
+      },
+      {
+        title: 'Rubah Asuransi',
+        route: null,
+      },
+      {
+        title: 'Laporan Penggunaan Helpdesk Tools',
+        route: null,
+      },
+      {
+        title: 'Rubah Transaksi Credit ke House Mail',
+        route: null,
+      },
+      {
+        title: 'Rubah Referensi Barang',
+        route: null,
+      },
+    ],
+  },
+]
