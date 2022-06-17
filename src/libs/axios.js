948baade6045ad43d5784b2056a085a400b2de99
ramafikri02 <ramafikri02@gmail.com>
2022-06-17 13:49:38 +0700

    Url API

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -6,8 +6,8 @@ import axios from 'axios'
 const axiosIns = axios.create({
   // You can add your headers here
   // ================================
-  // baseURL: 'https://api.systics.saranamulyalogisticscorp.com/api/',
-  baseURL: 'http://localhost:9999/api/',
+  baseURL: 'https://api.systics.saranamulyalogisticscorp.com/api/',
+  // baseURL: 'http://localhost:9999/api/',
   auth: {
     username: 'admin',
     password: '12345'
